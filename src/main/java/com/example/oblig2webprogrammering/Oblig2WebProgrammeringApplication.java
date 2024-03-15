9c9abab27d9e7e18a8c6809dd9e77fa2da2e23b4
peterandersenn <peand9319@oslomet.no>
2024-03-15 15:27:49 +0100

    k

---
+++ src/main/java/com/example/oblig2webprogrammering/Oblig2WebProgrammeringApplication.java
@@ -0,0 +1,13 @@
+package com.example.oblig2webprogrammering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig2WebProgrammeringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oblig2WebProgrammeringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oblig2webprogrammering/Oblig2WebProgrammeringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oblig2webprogrammering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig2WebProgrammeringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
